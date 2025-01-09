aaf3c8b761f9bbc57558b101ead75faabaa946da
Wilson Biggs <wab41@pitt.edu>
2025-01-09 14:32:11 -0500

    Working!

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
 const xhr = new XMLHttpRequest();
-xhr.open('GET', 'https://firefoxbug.free.beeceptor.com');
-xhr.overrideMimeType('text/html');
+xhr.open('GET', 'https://firefoxbug.free.beeceptor.com', false);
 xhr.send();
-console.log('XHR SENT', document.documentElement.innerHTML.slice(-1000));
